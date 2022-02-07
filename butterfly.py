2a05ebf8bc3c589d7475c649d536f955c1b78b5f
Nichole Boothman <nbooth731@gmail.com>
2022-02-07 14:46:35 -0500

    added new functions to create csv file of scraped data

--- butterfly.py
+++ butterfly.py
@@ -1,6 +1,8 @@
 # import libraries needed: requests and beautifulsoup.
 import requests 
 from bs4 import BeautifulSoup
+import re
+import csv
 
 r = requests.get('https://butterfly-conservation.org/uk-butterflies/a-to-z')
 # get request to specific webpage set to variable 'r'.
@@ -19,3 +21,49 @@ butterfly_pages = hrefs[39:100]
 # specifying we dont want every single link on the page only the ones that pertain to butterflies. Looked at the website and found it was from link 39 to 100.
 urls = ['https://butterfly-conservation.org/' + page for page in butterfly_pages]
 # created url variable with the base url = 'https://....' and then tack on the page or 'name of butterfly' for each butterfly page there is.
+
+def peel_data_from_element(element):
+  just_text = element.text
+  return just_text.split(': ')[1]
+
+def get_butterfly(url):
+    """Request and parse a single butterfly profile page, return a dict of data"""
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+
+    h1 = soup.find('h1')
+    name = h1.text
+    name = name.strip()
+    # strips off whitespace at end of name 
+
+    family = soup.find('li', text=re.compile(r'Family:*'))
+    size = soup.find('li', text=re.compile(r'Size:*'))
+    wing_span = soup.find('li', text=re.compile(r'Wing Span:*'))
+
+    return {'name' : name, 
+            'family': peel_data_from_element(family),
+            'size': peel_data_from_element(size),
+            'wing span': peel_data_from_element(wing_span),
+            'url': url}
+
+def process_each_link(url):
+    for url in urls:
+       list = get_butterfly(url)
+
+    return list
+
+def write_csv(list):
+    csv_content = list
+    csv_file = open('butterfly_data.csv', 'w')
+
+    csv_writer = csv.writer(csv_file, delimiter=',')
+
+    for row in csv_content:
+        csv_writer.writerow(row)
+    
+    csv_file.close()
+
+
+
+
